971230d43b0fcf5a6f92ac777321d8c3513eb410
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-02-05 02:21:10 +0400

    feat: implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -3,19 +3,39 @@ import socket
 import threading
 
 
+def parse_data(data: bytes) -> list[str]:
+    decoded_data: str = data.decode()
+
+    # Extract command
+    command = [arg for arg in decoded_data.split("\r\n")[:-1] if arg[0] not in "*$"]
+
+    return command
+
+
 def handle_connection(conn: socket.socket) -> None:
     PONG = "+PONG\r\n".encode()
 
-    while conn:
-        # Receive data from the client
-        data = conn.recv(1024)
+    # Receive data from the client
+    while data := conn.recv(1024):
         print(f"Received {data}")
 
         if not data:
             continue
 
-        # Send data back to the client
-        conn.send(PONG)
+        # Parse the data
+        command = parse_data(data)
+
+        # Respond based on command recieved
+        match command:
+            case ["ping"]:
+                response = PONG
+            case ["echo", data]:
+                response = f"+{data}\r\n".encode()
+            case _:
+                print("Unknown Command:", command)
+                response = f"-ERR unknown command '{command[0]}'\r\n".encode()
+
+        conn.sendall(response)
 
 
 def main() -> None:
