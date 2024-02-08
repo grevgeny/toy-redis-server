b7c82d2843d760e184d85e3550e0557bf28b6987
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-02-09 00:58:18 +0400

    refactor: implement better design

--- app/main.py
+++ app/main.py
@@ -1,77 +1,165 @@
 import asyncio
 import datetime
+import logging
 from typing import Any
 
-HOST, PORT = "", 6379
 
-PONG = b"+PONG\r\n"
-OK = b"+OK\r\n"
-NULL = b"$-1\r\n"
+class RedisDatabase:
+    def __init__(self):
+        self.data: dict[str, tuple[Any, datetime.datetime | None]] = {}
 
-DATABASE: dict[str, tuple[Any, datetime.datetime | None]] = {}
+    async def set(self, key: str, value: Any, expiry_ms: int | None = None) -> None:
+        """
+        Set the value of a key with an optional expiry time in milliseconds.
+        """
+        expiry = (
+            datetime.datetime.now(datetime.UTC)
+            + datetime.timedelta(milliseconds=expiry_ms)
+            if expiry_ms
+            else None
+        )
+        self.data[key] = (value, expiry)
+
+    async def get(self, key: str) -> Any | None:
+        """
+        Get the value of a key if it hasn't expired.
+        """
+        value, expiry = self.data.get(key, (None, None))
+        if expiry and expiry < datetime.datetime.now(datetime.timezone.utc):
+            await self.delete(key)  # Expire the key
+            return None
+        return value
+
+    async def delete(self, key: str) -> None:
+        """
+        Delete a key from the database.
+        """
+        if key in self.data:
+            del self.data[key]
 
 
-async def extract_command(data: bytes) -> list[str]:
-    decoded_data: str = data.decode()
-    command = [arg for arg in decoded_data.split("\r\n")[:-1] if arg[0] not in "*$"]
-    return command
+class CommandHandler:
+    def __init__(self, database: RedisDatabase) -> None:
+        self.database = database
+
+    async def handle_command(self, command: list[str]) -> bytes:
+        """
+        Dispatch the appropriate command to its handler and return the response.
+        """
+        if not command:
+            return b"-ERR no command provided\r\n"
+
+        cmd = command[0].lower()
+        args = command[1:]
+
+        if cmd == "ping":
+            return self.ping()
+        elif cmd == "echo":
+            return self.echo(args)
+        elif cmd == "set":
+            return await self.set(args)
+        elif cmd == "get":
+            return await self.get(args)
+        elif cmd == "del":
+            return await self.delete(args)
+        else:
+            return f"-ERR unknown command '{command[0]}'\r\n".encode()
+
+    def ping(self) -> bytes:
+        """
+        Simple PING command.
+        """
+        return b"+PONG\r\n"
+
+    def echo(self, args: list[str]) -> bytes:
+        """
+        ECHO command to return the message sent to it.
+        """
+        if not args:
+            return b"-ERR wrong number of arguments for 'echo' command\r\n"
+        message = " ".join(args)
+        return f"${len(message)}\r\n{message}\r\n".encode()
+
+    async def set(self, args: list[str]) -> bytes:
+        """
+        SET command to store a value.
+        """
+        if len(args) < 2:
+            return b"-ERR wrong number of arguments for 'set' command\r\n"
+        key, value = args[0], args[1]
+        expiry_ms = int(args[3]) if len(args) > 3 else None
+        await self.database.set(key, value, expiry_ms)
+        return b"+OK\r\n"
+
+    async def get(self, args: list[str]) -> bytes:
+        """
+        GET command to retrieve a value.
+        """
+        if len(args) != 1:
+            return b"-ERR wrong number of arguments for 'get' command\r\n"
+        key = args[0]
+        value = await self.database.get(key)
+        if value is None:
+            return b"$-1\r\n"
+        return f"${len(value)}\r\n{value}\r\n".encode()
+
+    async def delete(self, args: list[str]) -> bytes:
+        """
+        DEL command to delete one or more keys.
+        """
+        if not args:
+            return b"-ERR wrong number of arguments for 'del' command\r\n"
+        for key in args:
+            await self.database.delete(key)
+        return f":{len(args)}\r\n".encode()
 
 
-async def handle_connection(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+async def handle_connection(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter, db: RedisDatabase
+):
+    command_handler = CommandHandler(db)
     addr = writer.get_extra_info("peername")
-    print("Connected by", addr)
+    logging.info(f"Connected by {addr}")
+
     while True:
-        # Receive
         try:
             data = await reader.read(1024)
         except ConnectionError:
-            print(f"Client suddenly closed while receiving from {addr}")
+            logging.error(f"Client suddenly closed while receiving from {addr}")
             break
         if not data:
             break
 
-        # Parse the data and extract command
-        command = await extract_command(data)
-
-        # Respond based on command recieved
-        match command:
-            case ["ping"]:
-                response = PONG
-            case ["echo", data]:
-                response = f"+{data}\r\n".encode()
-            case ["set", key, value]:
-                DATABASE[key] = (value, None)
-                response = OK
-            case ["set", key, value, _, time]:
-                expiry = datetime.datetime.now(datetime.UTC) + datetime.timedelta(
-                    milliseconds=int(time)
-                )
-                DATABASE[key] = (value, expiry)
-                response = OK
-            case ["get", key]:
-                value, expiry = DATABASE.get(key, (None, None))
-                if expiry and expiry < datetime.datetime.now(datetime.UTC):
-                    DATABASE.pop(key)
-                    response = NULL
-                else:
-                    response = f"+{value}\r\n".encode() if value else NULL
-            case _:
-                response = f"-ERR unknown command '{command[0]}'\r\n".encode()
-
+        command = [
+            arg for arg in data.decode().split("\r\n")[:-1] if arg[0] not in "*$"
+        ]
+        response = await command_handler.handle_command(command)
         try:
             writer.write(response)
+            await writer.drain()
         except ConnectionError:
-            print("Client suddenly closed, cannot send")
+            logging.error("Client suddenly closed, cannot send")
             break
+
     writer.close()
-    print("Disconnected by", addr)
+    logging.info(f"Disconnected by {addr}")
 
 
-async def main(host, port):
-    server = await asyncio.start_server(handle_connection, host, port)
+async def main(host: str, port: int):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    db = RedisDatabase()
+    server = await asyncio.start_server(
+        lambda r, w: handle_connection(r, w, db), host, port
+    )
     async with server:
         await server.serve_forever()
 
 
 if __name__ == "__main__":
+    HOST, PORT = "127.0.0.1", 6379
     asyncio.run(main(HOST, PORT))
