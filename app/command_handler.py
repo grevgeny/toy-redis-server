109b12e316413f1af055d1071edfc35b290d35a4
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-02-24 22:38:18 +0400

    refactor: use factory for command handling

--- app/command_handler.py
+++ app/command_handler.py
@@ -1,84 +1,67 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
 from app.database import RedisDatabase
 
 
-class CommandHandler:
-    def __init__(self, database: RedisDatabase) -> None:
-        self.database = database
-        self.command_registry = {
-            "ping": self.ping,
-            "echo": self.echo,
-            "set": self.set,
-            "get": self.get,
-            "del": self.delete,
-            "config": self.config,
-            "keys": self.keys,
-            "info": self.info,
-        }
+@dataclass
+class Command(ABC):
+    database: RedisDatabase
+    args: list[str] = field(default_factory=list)
 
-    async def handle_command(self, command: list[str]) -> bytes:
-        if not command:
-            return b"-ERR no command provided\r\n"
+    @abstractmethod
+    async def execute(self) -> bytes:
+        pass
 
-        cmd, *args = command[1:]
 
-        handler = self.command_registry.get(cmd)
-        if handler:
-            return await handler(args)
-        else:
-            return f"-ERR unknown command '{cmd}'\r\n".encode()
-
-    async def ping(self, args: list[str]) -> bytes:
-        """
-        Simple PING command.
-        """
+class PingCommand(Command):
+    async def execute(self) -> bytes:
         return b"+PONG\r\n"
 
-    async def echo(self, args: list[str]) -> bytes:
-        """
-        ECHO command to return the message sent to it.
-        """
-        if not args:
+
+class EchoCommand(Command):
+    async def execute(self) -> bytes:
+        if not self.args:
             return b"-ERR wrong number of arguments for 'echo' command\r\n"
-        message = " ".join(args)
+        message = " ".join(self.args)
         return f"${len(message)}\r\n{message}\r\n".encode()
 
-    async def set(self, args: list[str]) -> bytes:
-        """
-        SET command to store a value.
-        """
-        if len(args) < 2:
+
+class SetCommand(Command):
+    async def execute(self) -> bytes:
+        if len(self.args) < 2:
             return b"-ERR wrong number of arguments for 'set' command\r\n"
-        key, value = args[0], args[1]
-        expiry_ms = int(args[3]) if len(args) > 3 else None
+        key, value = self.args[0], self.args[1]
+        expiry_ms = int(self.args[3]) if len(self.args) > 3 else None
         await self.database.set(key, value, expiry_ms)
         return b"+OK\r\n"
 
-    async def get(self, args: list[str]) -> bytes:
-        """
-        GET command to retrieve a value.
-        """
-        if len(args) != 1:
+
+class GetCommand(Command):
+    async def execute(self) -> bytes:
+        if len(self.args) != 1:
             return b"-ERR wrong number of arguments for 'get' command\r\n"
-        key = args[0]
+        key = self.args[0]
         value = await self.database.get(key)
         if value is None:
             return b"$-1\r\n"
         return f"${len(value)}\r\n{value}\r\n".encode()
 
-    async def delete(self, args: list[str]) -> bytes:
-        """
-        DEL command to delete one or more keys.
-        """
-        if not args:
+
+class DeleteCommand(Command):
+    async def execute(self) -> bytes:
+        if not self.args:
             return b"-ERR wrong number of arguments for 'del' command\r\n"
-        total_deleted = sum([await self.database.delete(key) for key in args])
+        total_deleted = sum([await self.database.delete(key) for key in self.args])
         return f":{total_deleted}\r\n".encode()
 
-    async def config(self, args: list[str]) -> bytes:
-        if not args:
+
+class ConfigCommand(Command):
+    async def execute(self) -> bytes:
+        if not self.args:
             return b"-ERR wrong number of arguments for 'config get' command\r\n"
-        if args[0] == "get":
-            key = args[1]
+        if self.args[0] == "get":
+            key = self.args[1]
             if key == "dir":
                 value = self.database.config.rdb_dir
             elif key == "dbfilename":
@@ -93,19 +76,20 @@ class CommandHandler:
         else:
             return b"-ERR wrong arguments for 'config' command\r\n"
 
-    async def keys(self, args: list[str]) -> bytes:
-        if not args:
+
+class KeysCommand(Command):
+    async def execute(self) -> bytes:
+        if not self.args or self.args[0] != "*":
             return b"-ERR wrong number of arguments for 'keys' command\r\n"
-        if args[0] == "*":
-            keys = await self.database.keys()
-            n = len(keys)
-            keys_array = [f"${len(key)}\r\n{key}\r\n" for key in keys]
-        else:
-            return b"-ERR wrong arguments for 'keys' command\r\n"
+        keys = await self.database.keys()
+        n = len(keys)
+        keys_array = [f"${len(key)}\r\n{key}\r\n" for key in keys]
         return f"*{n}\r\n{''.join(keys_array)}".encode()
 
-    async def info(self, args: list[str]) -> bytes:
-        if not args or args[0] != "replication":
+
+class InfoCommand(Command):
+    async def execute(self) -> bytes:
+        if not self.args or self.args[0] != "replication":
             return b"-ERR wrong arguments for 'info' command provided\r\n"
 
         role = "role:master" if not self.database.config.master_host else "role:slave"
@@ -118,3 +102,43 @@ class CommandHandler:
         response = "\r\n".join(response_parts)
 
         return f"${len(response)}\r\n{response}\r\n".encode()
+
+
+async def create_command(
+    command_name: str, args: list[str], database: RedisDatabase
+) -> Command:
+    match command_name:
+        case "ping":
+            return PingCommand(database)
+        case "echo":
+            return EchoCommand(database, args)
+        case "set":
+            return SetCommand(database, args)
+        case "get":
+            return GetCommand(database, args)
+        case "delete":
+            return DeleteCommand(database, args)
+        case "config":
+            return ConfigCommand(database, args)
+        case "keys":
+            return KeysCommand(database, args)
+        case "info":
+            return InfoCommand(database, args)
+        case _:
+            raise ValueError(f"Unknown command '{command_name}'")
+
+
+class CommandHandler:
+    def __init__(self, database: RedisDatabase) -> None:
+        self.database = database
+
+    async def handle_command(self, raw_command: list[str]) -> bytes:
+        if not raw_command:
+            return b"-ERR no command provided\r\n"
+
+        command_name, *args = raw_command[1:]
+        try:
+            command = await create_command(command_name, args, self.database)
+            return await command.execute()
+        except ValueError:
+            return f"-ERR unknown command '{command_name}'\r\n".encode()
