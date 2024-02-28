7c9ea9645f14cd0f8682f4ebf853793314445831
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-02-28 17:30:10 +0400

    refactor: prettify entrypoint setup

--- app/redis_config.py
+++ app/redis_config.py
@@ -1,17 +1,26 @@
-from dataclasses import dataclass
+import secrets
+from dataclasses import dataclass, field
+from enum import Enum, auto
+
+
+class Role(Enum):
+    MASTER = auto()
+    SLAVE = auto()
 
 
 @dataclass
 class RedisConfig:
-    role: str
+    host: str
+    port: int
     rdb_dir: str | None
     rdb_filename: str | None
     master_host: str | None
     master_port: int | None
-    port: int = 6379
-    master_replid: str = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
-    master_repl_offset: int = 0
+    role: Role
+    master_replid: str | None = field(default=None, init=False)
+    master_repl_offset: int | None = field(default=None, init=False)
 
     def __post_init__(self) -> None:
-        if self.master_port:
-            self.master_port = int(self.master_port)
+        if self.role == Role.MASTER:
+            self.master_replid = secrets.token_hex(40)
+            self.master_repl_offset = 0
--- main.py
+++ main.py
@@ -4,53 +4,67 @@ import logging
 
 # main.py
 from app.database import RedisDatabase
-from app.redis_config import RedisConfig
+from app.exceptions import ReplicationInitializationError
+from app.redis_config import RedisConfig, Role
 from app.server import start_server
 
 
-async def main() -> None:
+def create_redis_config_from_args() -> RedisConfig:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host", type=str, default="127.0.0.1", help="The host address to bind"
+    )
+    parser.add_argument("--port", type=int, default=6379, help="The port to listen on")
+    parser.add_argument(
+        "--dir", type=str, help="The directory where RDB files are stored"
+    )
+    parser.add_argument("--dbfilename", type=str, help="The name of the RDB file")
+    parser.add_argument(
+        "--replicaof",
+        nargs=2,
+        metavar=("MASTER_HOST", "MASTER_PORT"),
+        help="Master host and master port for the replica.",
+    )
+    args = parser.parse_args()
+    master_host, master_port = args.replicaof if args.replicaof else (None, None)
+    role = Role.SLAVE if master_host else Role.MASTER
+
+    return RedisConfig(
+        host=args.host,
+        port=args.port,
+        role=role,
+        rdb_dir=args.dir,
+        rdb_filename=args.dbfilename,
+        master_host=master_host,
+        master_port=int(master_port) if master_port else None,
+    )
+
+
+async def main(config: RedisConfig) -> None:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    config = parse_args()
-    db = RedisDatabase(config=config)
+    db = None
+    if config.role == Role.SLAVE:
+        try:
+            db = await RedisDatabase.init_slave(config=config)
+        except ReplicationInitializationError as e:
+            logging.error(f"Failed to start as a slave: {e}")
+    else:
+        db = RedisDatabase.init_master(config=config)
 
-    host = "127.0.0.1"
-    try:
-        await start_server(host=host, port=config.port, db=db)
-    finally:
-        await db.close()
-
-
-def parse_args() -> RedisConfig:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=6379, help="The port to listen on")
-    parser.add_argument(
-        "--replicaof",
-        nargs=2,
-        metavar=("MASTER_HOST", "MASTER_PORT"),
-        default=[None, None],
-        help="Master host and master port for the replica.",
-    )
-    parser.add_argument(
-        "--dir", type=str, help="The directory where RDB files are stored"
-    )
-    parser.add_argument("--dbfilename", type=str, help="The name of the RDB file")
-
-    args = parser.parse_args()
-
-    return RedisConfig(
-        port=args.port,
-        rdb_dir=args.dir,
-        rdb_filename=args.dbfilename,
-        master_host=args.replicaof[0],
-        master_port=args.replicaof[1],
-        role="slave" if args.replicaof[0] else "master",
-    )
+    if db:
+        try:
+            await start_server(host=config.host, port=config.port, db=db)
+        finally:
+            await db.close()
+    else:
+        logging.error("Database initialization failed. Exiting.")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    config = create_redis_config_from_args()
+    asyncio.run(main(config))
