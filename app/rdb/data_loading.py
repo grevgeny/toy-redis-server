698032d585de0f776a7e22c19d9916f2065f855c
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-03-08 20:42:25 +0400

    refactor: do not pass whole config to data loader

--- app/rdb/data_loading.py
+++ app/rdb/data_loading.py
@@ -2,16 +2,13 @@ import datetime
 import os
 
 from app.rdb.parser import RDBParser
-from app.redis_config import RedisConfig
 
 Data = dict[str, tuple[str, datetime.datetime | None]]
 
 
-def load_init_data_for_master(
-    config: RedisConfig,
-) -> Data:
-    if config.rdb_dir and config.rdb_filename:
-        file_path = os.path.join(config.rdb_dir, config.rdb_filename)
+def load_init_data_for_master(rdb_dir: str | None, rdb_filename: str | None) -> Data:
+    if rdb_dir and rdb_filename:
+        file_path = os.path.join(rdb_dir, rdb_filename)
         if os.path.exists(file_path):
             return RDBParser.load_from_file(file_path)
     return {}
